345d62df2a6a917190aa896dfcb0ec9681be3937
jfrizzar <jfrizzar@gmail.com>
2025-07-15 12:26:19 -0400

    Added methods to be used and associated psuedo code. Also created a CSV file with events to be used in the project

--- GMUEventTracker.java
+++ GMUEventTracker.java
@@ -1,11 +1,24 @@
-import java.util.Scanner;
-
 public class GMUEventTracker {
+
+    public static void readCSV(String fileName){
+        /*This method will read a CSV file and write into a string array*/
+    }
+
+    public static void eventSelector(String campusEvents[]){
+        /*This method will take our array of campus events as inputs and loop while user is selecting an event*/
+    }
 
     public static void main(String args[]){
 
-        Scanner scnr = new Scanner(System.in);
 
-        
+        /*Open a CSV File
+        Read CSV file into array making sure to include the index number so that users can select it, Array will be of type string
+        Show top trending events at the top by assigning them trending boolean value true
+        Show rest of the available events
+        While true, scan a user integer
+        Use integer to register user for the event they are interested in, only let student enroll once using true/false boolean
+        Use a Try/Exception for all invalid inputs (out of range, not an int)
+        If valid input is given give user feedback that they have succesfully registered for desired event, if invalid display error
+        */
     } 
 }
