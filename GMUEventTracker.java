7afeed2daaa6acdb9e72772ffec728e4c4743df9
Jamil Frizzar <jfrizzar@gmail.com>
2025-07-17 14:57:06 -0400

    Created the method which reads a CSV file, prints each event in an organized manner and loads each event into an array for future use in the eventSelector method

--- GMUEventTracker.java
+++ GMUEventTracker.java
@@ -1,7 +1,33 @@
+import java.io.*;
+import java.util.*;
+
 public class GMUEventTracker {
 
-    public static void readCSV(String fileName){
+    public static String[] readCSV(String fileName){
         /*This method will read a CSV file and write into a string array*/
+        int count = 0;
+        List<String> lines = new ArrayList<>();
+        String allEvents[] = {};
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){ //Create Buffered reader and check for exceptions
+            String line;
+            line = reader.readLine(); //Skip over first line of CSV file
+            while ((line = reader.readLine()) != null){ //Assign the string line with the current value of our reader and loops until it reaches null
+                String[] currentEvent = line.split(",");
+                System.out.println(currentEvent[0] + ": " + currentEvent[7] + "\nDate: " + currentEvent[3] + "\nTime: " + currentEvent[4] + "\nCurrent Attendance: " + currentEvent[5] + "\nOpen Slots: " + currentEvent[6] + "\nLocation: " + currentEvent[2]);
+                System.out.println();
+                lines.add(line);
+                allEvents = lines.toArray(new String[count]);
+                count++;
+            }
+        }
+        catch(FileNotFoundException fnfe){ //Check if there is a file available
+            System.out.println("File not found, please try again.");
+        }
+        catch(IOException ioe){ //Check if there is an input/output exception
+            System.out.println("Input/output error, please try again.");
+        }
+        return allEvents;
     }
 
     public static void eventSelector(String campusEvents[]){
@@ -10,6 +36,8 @@ public class GMUEventTracker {
 
     public static void main(String args[]){
 
+        String fileName = "GMUEvents.csv"; //Define the filename to be opened
+        String allEvents[] = readCSV(fileName); //Uses read CSV function to get an array of all the events
 
         /*Open a CSV File
         Read CSV file into array making sure to include the index number so that users can select it, Array will be of type string
